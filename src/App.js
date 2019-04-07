34c4ad93a423163be9ee109f9a11ecd9146ef4b1
Arno-Macbook <arno.welgemoed@outlook.com>
2019-04-07 17:23:17 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserIsAuthenticated } from './helpers';
 import AddProject from './components/AddProject';
 import Footer from './components/Footer';
