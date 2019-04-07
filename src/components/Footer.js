fd43459730b920d81b755cbeede503492c7fbb65
Arno-Macbook <arno.welgemoed@outlook.com>
2019-04-07 19:44:02 +0200

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Footsy = styled.footer`
   background: ${props => props.theme.footy};
@@ -29,7 +30,9 @@ export default function Footer() {
               </h6>
             </div>
             <div className="col-md-4 col-lg-4 col-sm-12  text-md-left mb-4 mb-md-0">
-              <h6 className="mb-0 footer-Right ">code by AH Welgemoed</h6>
+              <h6 className="mb-0 footer-Right ">
+                <Link to="/leaslogin">code by AH Welgemoed</Link>
+              </h6>
             </div>
           </div>
         </div>
