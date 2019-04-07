b671229fd2bb05a9111ebd6393cacf1f0ba8779a
Arno-Macbook <arno.welgemoed@outlook.com>
2019-04-07 17:35:22 +0200

    test Deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserIsAuthenticated } from './helpers';
 import AddProject from './components/AddProject';
 import Footer from './components/Footer';
